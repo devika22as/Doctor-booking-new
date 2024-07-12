cc458582087b7a360400d24624fa79417ad48409
Shalu99v <shalu99v@gmail.com>
2024-07-12 11:04:22 +0530

    ADDING ONITIAL FOLDERS

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express= require('express');
+const cors=require('cors')
+const dotenv=  require('dotenv')
+const app= express();
+const db=require('./db')
+
+dotenv.config({path:'./.env'})
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static('public'))
+
+const routes= require('./routes');
+app.use(routes);
+
+app.listen(process.env.PORT,()=>{
+    console.log("App is running...")
+    console.log(`http://localhost:${process.env.PORT}/`)
+})
